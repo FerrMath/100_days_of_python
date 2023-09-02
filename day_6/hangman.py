e122bdc7803fbc6c0c50f88119e8a781de097aea
Matheus A <maf_dev@hotmail.com>
2023-09-02 17:20:18 -0300

    add logic to verify if user wins the game

--- day_6/hangman.py
+++ day_6/hangman.py
@@ -3,11 +3,7 @@ from hangman_art import stages, logo
 from palavras import palavras_aleatorias as p
 
 
-
 def play():
-    # Gerando área de input do usuario
-    entradas = []
-
     # Quantidade de vidas restantes
     vidas = len(stages)
 
@@ -17,22 +13,29 @@ def play():
     # Letras escolhidas pelo usuario
     letras = set()
 
+    print(logo,"\n")
+
     # Loop principal do jogo
-    # TODO criar sistema de vitória caso o jogar acerte a palavra
     while True:
         letra = input("Digite uma letra: ").lower()
         letras.add(letra)
+        
         if letra not in palavra_secreta:
             vidas = reduzir_vida(vidas)
-            print(vidas)
             mostrar_erro(vidas, letra)
             if vidas == 0:
-                fim_de_jogo()
+                fim_de_jogo_derrota(palavra_secreta)
                 return
-        exibir_tentativa(letras, palavra_secreta)
+            exibir_tentativa(letras, palavra_secreta)
+        else:
+            if acertou_palavra(palavra_secreta, letras):
+                fim_de_jogo_vitoria(palavra_secreta)
+                return
+            exibir_tentativa(letras, palavra_secreta)
 
 
 def exibir_tentativa(tentativas:set[str], palavra):
+    print()
     entradas = ["_" if letra not in tentativas else letra for letra in palavra]
     print("".join(entradas))
 
@@ -42,13 +45,24 @@ def reduzir_vida(vidas):
 
 
 def mostrar_erro(vidas: int, letra: str):
-    print(vidas)
     print(stages[vidas])
     print(f"A letra \"{letra}\" não está na palavra")
     print()
 
 
-def fim_de_jogo(palavra: str):
+def acertou_palavra(palavra, letras):
+    tentativa = ["_" if letra not in letras else letra for letra in palavra]
+    return "".join(tentativa) == palavra
+
+
+def fim_de_jogo_vitoria(palavra:str):
+    print()
+    print("Fim de jogo, você ganhou!")
+    print(f"A palavra era: {palavra.title()}!")
+
+
+def fim_de_jogo_derrota(palavra: str):
+    print()
     print("Fim de jogo, você perdeu!")
     print(f"A palavra era: {palavra.title()}!")
 
